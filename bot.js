159ef24540b64a3703c11cf2e3fdab11265a2e70
maxnachemson <maxna@kth.se>
2017-04-11 18:53:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,59 +1,158 @@
-var STUDIO_TOKEN = process.env.BOTKIT_STUDIO_TOKEN
-var SLACK_TOKEN = process.env.SLACK_TOKEN
-
-var Botkit = require('botkit')
-var fs = require('fs')
-var path = require('path')
-var debug = require('debug')('botkit:main')
-var BotkitStorageBeepBoop = require('botkit-storage-beepboop')
+var Botkit = require('botkit');
+var controller = Botkit.slackbot();
+var SLACK_TOKEN = process.env.SLACK_TOKEN;
 var request = require('request');
 
+var options = {
+      url: 'https://script.googleusercontent.com/macros/echo?user_content_key=jQ2TC03qiZkWyvx07u8gzYWZiYtUzF5o8tkeG7unQdn2iQlye3fjmIQoIVzFfMcCSQe0qR3yPyNLONDRdDfs9Ww3xagBWEXcOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMWojr9NvTBuBLhyHCd5hHa1ZsYSbt7G4nMhEEDL32U4DxjO7V7yvmJPXJTBuCiTGh3rUPjpYM_V0PJJG7TIaKp1E_BOymz-tQ-8TxUtrLWLSKPKjFjBmpg0EpTpFurEIVpbQEzawfLw93LnxErnS8iQ&lib=MbpKbbfePtAVndrs259dhPT7ROjQYJ8yx'
+};
 
-// Create the Botkit controller, which controls all instances of the bot.
-var controller = Botkit.slackbot({
-  debug: false,
-  studio_token: STUDIO_TOKEN,
-  slack_token: SLACK_TOKEN,
-  storage: BotkitStorageBeepBoop()
-})
-controller.startTicking()
+function callback(error, response, body) {
+    console.log("Downloading database...");
+    if (!error && response.statusCode == 200) {
+        info = JSON.parse(body);
+    }
+    console.log("Database OK");
+    return info;
+}
 
-// Set up an Express-powered webserver to expose oauth and webhook endpoints
-require('./components/webserver/')(controller)
+//request(options, callback);
 
-// Register sample "skills"
-var normalizedPath = path.join(__dirname, 'skills')
-fs.readdirSync(normalizedPath).forEach(file => {
-  require(path.join(normalizedPath, file))(controller)
-})
+    
+    
 
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (STUDIO_TOKEN) {
-  controller.on('direct_message,direct_mention,mention', (bot, message) => {
-    controller.studio.runTrigger(bot, message.text, message.user, message.channel).then(convo => {
-      if (!convo) {
-        // no trigger was matched
-        // If you want your bot to respond to every message,
-        // define a 'fallback' script in Botkit Studio
-        // and uncomment the line below.
-        // controller.studio.run(bot, 'fallback', message.user, message.channel)
-      } else {
-        // set variables here that are needed for EVERY script
-        // use controller.studio.before('script') to set variables specific to a script
-        convo.setVar('current_time', new Date())
-      }
-    }).catch(err => {
-      bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err)
-      debug('Botkit Studio: ', err)
+controller.hears(["Hello","Hey","Hi","Yo"],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+    bot.reply(message,'Hi there!');
+    console.log("Incoming message: "+message.text);
+});
+
+controller.hears([/^.{0,}Thank.{0,}$/, /^.{0,}thx.{0,}$/],["direct_message","direct_mention","mention","ambient"],function(bot,message) {
+    bot.reply(message,'No problems!');
+    console.log("Incoming message: "+message.text);
+});
+
+//
+// case sensetive
+// category list
+
+controller.hears([/^.{0,}job.{0,}$/], ["direct_message","direct_mention","mention","ambient"], function (bot, message) {
+  request(options, callback);
+  console.log("Incoming message: "+message.text);
+  bot.startConversation(message, function (err, convo) {
+    convo.ask('What kind of job are you looking for?', function (response, convo) {
+        
+        theData = info['Blad1'];
+        console.log("Total jobs in database: "+theData.length);
+        console.log("Search term: "+response.text);
+        var results = [];
+        toSearch = response.text;
+        categoryArr = [];
+        //Loop through the data from the database
+        for(var i=0; i<theData.length; i++) {
+          categoryArr[i] = theData[i].category;
+          for(key in theData[i]) {
+            if(theData[i][key].includes(toSearch)) {
+              results.push(theData[i]);
+              break;
+            }
+          }
+        }
+
+        //Filter category array & create message
+        var categories = categoryArr.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
+        var categoryMsg = "";
+        for(var i=0; i<categories.length; i++) {
+            categoryMsg += "*"+categories[i]+"*\n";
+        }
+
+        //Construct respond message
+        var resultMessage = {
+            "attachments":[]
+        };
+        var row = 0;
+        for(var i=0; i<results.length; i++) {
+            row++;
+            console.log(row+" match");
+            newrow = '';
+            newrow = { 
+                "fallback": "jobs jobs jobs",
+                "color":"#36a64f",
+                "title": " "+results[i].title+" @"+results[i].company+" ",
+                "title_link": results[i].link
+        };
+        resultMessage['attachments'].push(newrow);
+        }
+
+        
+        //Send respond message
+        if (results.length > 0) {
+            if (row === 1) {
+                var ending = "";
+            } else {
+                var ending = "s";
+            }
+            convo.say("I found "+row+" opening"+ending+":");
+            convo.say(resultMessage);
+        } else {
+            console.log("0 match");
+            convo.say("I couldn't find any jobs related to _"+toSearch+"_");
+            convo.ask('Try narrowing it down by choosing a category: \n'+categoryMsg, function (response, convo) {
+                toSearch = response.text;
+                categoryArr = [];
+                //Loop through the data from the database
+                for(var i=0; i<theData.length; i++) {
+                  categoryArr[i] = theData[i].category;
+                  for(key in theData[i]) {
+                    if(theData[i][key].includes(toSearch)) {
+                      results.push(theData[i]);
+                      break;
+                    }
+                  }
+                }
+                //Construct respond message
+                var resultMessage = {
+                    "attachments":[]
+                };
+                var row = 0;
+                for(var i=0; i<results.length; i++) {
+                    row++;
+                    console.log(row+" match");
+                    newrow = '';
+                    newrow = { 
+                        "fallback": "jobs jobs jobs",
+                        "color":"#36a64f",
+                        "title": " "+results[i].title+" @"+results[i].company+" ",
+                        "title_link": results[i].link
+                };
+                resultMessage['attachments'].push(newrow);
+                }
+                //Send respond message
+                if (results.length > 0) {
+                    if (row === 1) {
+                        var ending = "";
+                    } else {
+                        var ending = "s";
+                    }
+                    convo.say("I found "+row+" opening"+ending+":");
+                    convo.say(resultMessage);
+                }
+                convo.next();
+            });
+
+        }
+
+      convo.next() // always call this to keep things flowing (check the readme for more info)
     })
   })
-} else {
-  console.log('~~~~~~~~~~')
-  console.log('NOTE: Botkit Studio functionality has not been enabled')
-  console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/')
-}
+})
+
+var bot = controller.spawn ({
+    token: SLACK_TOKEN
+});
+
+bot.startRTM(function(err, bot, payload) {
+    if (err) {
+        console.log(err);
+        throw new Error('Could not connect to Slack');
+    }
+});
